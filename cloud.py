50b541eccfd0359465cc9f7730ebb7572ba35d89
ctang <chao.tang.1@gmail.com>
2022-09-03 22:15:07 +0400

    major update: add all data and redo all analysis.

--- cloud.py
+++ cloud.py
@@ -40,6 +40,7 @@ def cloud(cfg: DictConfig) -> None:
 
     # calculate necessary data and save it for analysis:
     # ==================================================================== data:
+
     if any(GEO_PLOT.get_values_multilevel_dict(dict(cfg.job.data))):
 
         # -------------------------------------- SAF_NWC ---------------------
@@ -69,7 +70,6 @@ def cloud(cfg: DictConfig) -> None:
 
                 # list_file: list = glob.glob(f'{cfg.dir.icare_data_ccur:s}/ct.*Z.nc')
                 list_file: list = glob.glob(f'/gpfs/scratch/le2p/OBS_DATA/icare/ctang/ct.*Z.nc')
-
 
                 # resort by DateTime in the name file
                 list_file.sort()
@@ -114,7 +114,7 @@ def cloud(cfg: DictConfig) -> None:
 
             for year in [2017, 2018, 2019, 2020, 2021, 2022]:
                 path = f'/gpfs/scratch/le2p/OBS_DATA/icare/ctang/{str(year):s}'
-                list_lonlat_file: list = glob.glob(f'{path:s}/ct.*Z.lonlat.nc')
+                list_lonlat_file: list = glob.glob(f'{path:s}/ct.*_{str(year):s}*T*Z.lonlat.nc')
 
                 # resort by DateTime in the name file
                 list_lonlat_file.sort()
@@ -135,12 +135,94 @@ def cloud(cfg: DictConfig) -> None:
                 # after selection the Reunion domain still in general projection.
                 # see plots of latitude and longitude in ./plot
 
-                # merge reu (yearly)
+                # merge all reu file
                 list_reu_file: list = glob.glob(f'{path:s}/'
-                                            f'ct.*_{str(year):s}????T*Z.lonlat.reu.nc')
+                                                f'ct.*_????????T*Z.lonlat.reu.nc')
+
+                # lat become to 31 values from 30, since this file:
+                # ct.S_NWC_CT_MSG1_globeI-VISIR_20180911T110000Z.lonlat.reu.nc
+
                 list_reu_file.sort()
                 GEO_PLOT.nc_mergetime(list_reu_file, 'ct', output_tag='yearly')
                 # this file is saved on CCuR as: ct.S_NWC_CT_MSG1_globeI-VISIR_20190101T000000Z.lonlat.reu.yearly.nc
+
+        if cfg.job.data.merge_reu:
+            # download "yearly" file from CCuR, then merge them:
+            # file after selecting reu on CCuR, got these in local_data/reu_ccur:
+
+            # group 1:
+            a = f'{cfg.dir.local_data:s}/reu_ccur/ct.S_NWC_CT_MSG1_globeI-VISIR_20170601T080000Z.lonlat.reu.yearly.nc'
+            # 20170601 - 20180911, 34*30 (x*y) grid
+
+            # group 2:
+            b = f'{cfg.dir.local_data:s}/reu_ccur/ct.S_NWC_CT_MSG1_globeI-VISIR_20180911T110000Z.lonlat.reu.yearly.nc'
+            # 20180911 - 20181231, 34*31 grid
+            c = f'{cfg.dir.local_data:s}/reu_ccur/ct.S_NWC_CT_MSG1_globeI-VISIR_20190101T000000Z.lonlat.reu.yearly.nc'
+            # 20190101-20191231, 34*31 grid, lon/lat same with b
+            d = f'{cfg.dir.local_data:s}/reu_ccur/ct.S_NWC_CT_MSG1_globeI-VISIR_20200101T000000Z.lonlat.reu.yearly.nc'
+            # 20200101 - 20200630, 34*31 grid, lon/lat same with c
+
+            # group 3:
+            e = f'{cfg.dir.local_data:s}/reu_ccur/ct.S_NWC_CT_MSG1_globeI-VISIR_20200701T000000Z.lonlat.reu.yearly.nc'
+            # 20200701-20201231, 34*31 grid, lon/lat diff from file above, i.e. df, dif ~ 0.08 < reso = 0.03
+            f = f'{cfg.dir.local_data:s}/reu_ccur/ct.S_NWC_CT_MSG1_globeI-VISIR_20210101T000000Z.lonlat.reu.yearly.nc'
+            # 20210101-20211231, 34*31 grid, lon/lat same as above, ef.
+            g = f'{cfg.dir.local_data:s}/reu_ccur/ct.S_NWC_CT_MSG1_globeI-VISIR_20220101T000000Z.lonlat.reu.yearly.nc'
+            # 20220101-20220623, 34*31 grid, lon/lat same as above, ef.
+
+            gp1 = GEO_PLOT.read_to_standard_da(a, 'ct')
+            gp2 = GEO_PLOT.nc_mergetime(list_file=[b, c, d], var='ct', output_tag='mergetime', save=False)
+            gp3 = GEO_PLOT.nc_mergetime(list_file=[e, f, g], var='ct', output_tag='mergetime', save=False)
+
+            # merge 1: gp4 = gp2 + gp3, using coords from gp3:
+
+            # check if da1819 and da2122 has similar coords: << 0.5 * resolution
+            reso = 0.03
+
+            lon_max = np.abs((gp2.lon - gp3.lon).max())
+            lat_max = np.abs((gp2.lat - gp3.lat).max())
+            print(lon_max, lat_max)
+
+            if np.logical_and(lon_max < reso, lat_max < reso):
+                new_coords_param = {
+                    "time": gp2.time, "lat": (['y', 'x'], gp3.lat.values), "lon": (['y', 'x'], gp3.lon.values)}
+                new_gp2 = xr.DataArray(gp2.data, dims=list(gp3.dims), coords=new_coords_param, name=gp3.name,
+                                       attrs=gp3.attrs)
+
+            # merge gp4:
+            gp4 = xr.concat([new_gp2, gp3], dim='time')
+
+            # merge: gp5 = gp1 + gp4
+
+            # gp1 has less in lat, so select less lat from gp4  as new_gp4
+
+            # check lat/lon:
+            dif_max_lat = np.abs((gp4[:, 1:, :].lat - gp1.lat)).max()
+            dif_max_lon = np.abs((gp4[:, 1:, :].lon - gp1.lon)).max()
+
+            if np.logical_and(dif_max_lat < reso, dif_max_lon < reso):
+                new_gp4 = gp4[:, 1:, :]
+
+                # then give gp1 the coords of new_gp4, as new_gp1
+                new_coords_param = {
+                    "time": gp1.time,
+                    "lat": (['y', 'x'], new_gp4.lat.values),
+                    "lon": (['y', 'x'], new_gp4.lon.values)}
+
+                new_gp1 = xr.DataArray(gp1.data, dims=list(new_gp4.dims), coords=new_coords_param,
+                                       name=new_gp4.name, attrs=new_gp4.attrs)
+
+                # merge:
+                gp5 = xr.concat([new_gp1, new_gp4], dim='time')
+                gp5 = gp5.assign_attrs({'units': 'unitless'})
+
+                # shift time
+                gp5_local = GEO_PLOT.convert_da_shifttime(gp5, 3600 * 4)
+
+                # save it to dataset
+                gp5_local.to_netcdf(cfg.file.reu_localtime_2017_2022)
+
+            print(f'done')
 
         if cfg.job.data.mergetime_swio:
             # since CDO mergetime function will lose the lon/lat by unknown reason,
@@ -166,10 +248,9 @@ def cloud(cfg: DictConfig) -> None:
                 list_file.sort()
                 GEO_PLOT.nc_mergetime(list_file, 'ct', output_tag='monthly')
 
-
         if cfg.job.data.missing_reu:
             # read nc in UTC00
-            reu_da = GEO_PLOT.read_to_standard_da(cfg.file.reu_nc, 'ct')
+            reu_da = GEO_PLOT.read_to_standard_da(cfg.file.reu_localtime_2017_2022, 'ct')
 
             # remove maps with only nan values
             reu_da_nan_maps = reu_da.where(np.isnan(reu_da).all(dim={'x', 'y'}), drop=True)
@@ -179,24 +260,18 @@ def cloud(cfg: DictConfig) -> None:
                 # save it:
                 reu_da.to_netcdf(cfg.file.reu_nc)
 
-            # save local time file:
-            reu_da = GEO_PLOT.read_to_standard_da(cfg.file.reu_nc, 'ct')
-            reu_da_local_time = GEO_PLOT.convert_da_shifttime(da=reu_da, second=3600 * 4)
-            reu_da_local_time.to_netcdf(cfg.file.reu_local_time_nc)
-
             # check missing for each year
             freq = '15min'
-            start = '2019-01-01 00:00'
-            end = '2019-12-31 23:45'
+            start = '2017-06-01 12:00'
+            end = '2022-06-23 11:30'
 
-            mon_hour_matrix = GEO_PLOT.check_missing_da(
+            mon_hour_matrix = GEO_PLOT.check_missing_da_df(
                 start=start, end=end, freq=freq,
-                da=reu_da, plot=True)
-            print(mon_hour_matrix)
+                data=reu_da, plot=True)
 
         if cfg.job.data.select_moufia:
             # read utc reu nc:
-            reu = GEO_PLOT.read_to_standard_da(cfg.file.reu_nc, 'ct')
+            reu = GEO_PLOT.read_to_standard_da(cfg.file.reu_localtime_2017_2022, 'ct')
 
             Project_cloud.test_plot_reu_grid(reu)
 
@@ -207,8 +282,7 @@ def cloud(cfg: DictConfig) -> None:
             new_da = new_da.assign_attrs({'units': '', 'long_name': 'cloud_type'})
             new_da.to_netcdf(cfg.file.moufia_nc)  # UTC not local time
 
-            df_utc = new_da.to_dataframe()
-            df_local = GEO_PLOT.convert_df_shifttime(df_utc, 3600 * 4)
+            df_local = new_da.to_dataframe()
             df_local.to_pickle(cfg.file.moufia_local_time)
 
     # ==================================================================== moufia
@@ -225,7 +299,7 @@ def cloud(cfg: DictConfig) -> None:
             # local time data:
             moufia_raw = pd.read_pickle(cfg.file.moufia_local_time)
 
-            reu = GEO_PLOT.read_to_standard_da(cfg.file.reu_local_time_nc, 'ct')
+            reu = GEO_PLOT.read_to_standard_da(cfg.file.reu_localtime_2017_2022, 'ct')
 
             # select moufia with the neighbour 8 pixels:
             moufia_9p = GEO_PLOT.select_pixel_da(da=reu, lon=55.45, lat=-21.0, n_pixel=9)
@@ -251,7 +325,7 @@ def cloud(cfg: DictConfig) -> None:
 
             # see the spatial concurrency: monthly
             Project_cloud.plot_monthly_hourly_bar_unstack(df=frac_cld_df,
-                                                          title=f'ct in 2019, 8 moufia nearby pixels, '
+                                                          title=f'ct 201706 - 202206, 8 moufia nearby pixels, '
                                                                 f'when center = 10, @15min',
                                                           output_tag=f'8 pixels around moufia')
 
@@ -314,7 +388,7 @@ def cloud(cfg: DictConfig) -> None:
             print(f'cloud free sea = {len(moufia_reallocated[moufia_reallocated == 2].dropna()):g} days')
             print(f'snow over land = {len(moufia_reallocated[moufia_reallocated == 3].dropna()):g} days')
 
-            # remove snow over land, since wrong detections, since moufia nearly never has snow.
+            # remove snow over land, since wrong detections, since moufia nearly never has snow. while 83 found
             da1 = da1[da1 != 3].dropna()
 
             # make #11-15 to #11 as high semitransparent cloud
@@ -328,17 +402,16 @@ def cloud(cfg: DictConfig) -> None:
         if cfg.job.moufia.statistics:
             # load data for analysis:
             df = pd.read_pickle(cfg.file.moufia_reallocation_regroup)
-            df19 = df[df.index.year == 2019]  # some timestep are shift to 2020 after change the timezone
 
-            Project_cloud.plot_monthly_hourly_bar_unstack(df=df19,
+            Project_cloud.plot_monthly_hourly_bar_unstack(df=df,
                                                           title=f'moufia after reallocation regroup',
                                                           output_tag=f'moufia_reallocated_regroup')
 
             # plot all cloud types without clearsky:
-            df19_cld = df19[df19 != 1].dropna()
+            df_cld = df[df != 1].dropna()
 
-            Project_cloud.plot_monthly_hourly_bar_unstack(df=df19_cld, stack_full=True,
-                                                          title=f'moufia after reallocation regroup',
+            Project_cloud.plot_monthly_hourly_bar_unstack(df=df_cld, stack_full=True,
+                                                          title=f'moufia after reallocation regroup 201706-202206',
                                                           output_tag=f'moufia_reallocated_regroup_only_cloudy')
 
             # statistics:
@@ -359,11 +432,9 @@ def cloud(cfg: DictConfig) -> None:
         if any(GEO_PLOT.get_values_multilevel_dict(dict(cfg.job.moufia.correlations))):
 
             df = pd.read_pickle(cfg.file.moufia_reallocation_regroup)
-            df19 = df[df.index.year == 2019]  # some timestep are shift to 2020 after change the timezone
 
             if cfg.job.moufia.correlations.mean_annual_cycle:
                 # mean SSR annual cycle:
-
                 rg = GEO_PLOT.read_csv_into_df_with_header(cfg.file.gillot_rg_mf_2019)[{'GLO'}] * 10000 / 3600
                 # unit of mf is J/m2 (accumulated in 1 hour)
 
@@ -378,7 +449,7 @@ def cloud(cfg: DictConfig) -> None:
                 # correlation matrix:
                 # df with all necessary columns in 12 months or 24 hours
 
-                ct_annual = Project_cloud.annual_cycle_cloudiness(df19, '2019')
+                ct_annual = Project_cloud.annual_cycle_cloudiness(df, '201706-202206')
                 cld_ssr_monmean = pd.concat([ct_annual, ssr_day_night_monmean], axis=1)
 
                 # pearson:
@@ -392,9 +463,6 @@ def cloud(cfg: DictConfig) -> None:
                                            cbar_label='jj')
                 plt.savefig(f'./plot/annual_ct_ssr_cross_corr.png', dpi=300)
                 plt.show()
-
-
-
 
 
 if __name__ == "__main__":
